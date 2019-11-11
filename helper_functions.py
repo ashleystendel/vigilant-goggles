b9f94f32b4a3e46488b9f4d3adb9f0e89bc23693
Jake Mooney <jrmooney@gmail.com>
2019-11-11 10:58:00 +0200

    adding helper functions

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+from question_summary import *
+from user import *
+
+def find_and_populate(parser, html_tag, label, class_name):
+    collection = []
+    klass = globals()[class_name]
+    matches = parser.get_matches(html_tag, label)
+    for match in matches:
+        item = klass(parser)
+        item.scrape_info(match)
+        collection.append(item)
+    return collection
