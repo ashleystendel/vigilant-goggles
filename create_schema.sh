7c20cc92a3ba1453d975f0c433937d515e1ef4f7
Ashley Stendel <ashley.stendel@mac.com>
2019-12-02 16:37:12 +0200

    Fix create schema script

--- create_schema.sh
+++ create_schema.sh
@@ -2,9 +2,10 @@
 
 echo "Creating user itc..."
 
-PASS="$(cat password.txt)" envsubst < create_user.sql > create_user-tmp.sql
-mysql --datadir='~/test/' -h 127.0.0.1 -P 3306 -u root -p < create_user-tmp.sql
+PASS=$(cat password.txt) 
+PASS="$PASS" envsubst < create_user.sql > create_user-tmp.sql
+mysql -h 127.0.0.1 -P 3306 -u root -p < create_user-tmp.sql
 rm create_user-tmp.sql
 
 echo "Creating schema..."
-mysql --datadir='~/test/' -h 127.0.0.1 -P 3306 -u itc -p < create_table.sql
+mysql -h 127.0.0.1 -P 3306 -u itc --password="$PASS" < create_table.sql
