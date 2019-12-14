8b91dbc117bec9a01f2431df579f2cc0ccdd3ca8
Ashley Stendel <ashley.stendel@mac.com>
2019-12-14 20:32:20 +0200

    Add article table to create DB script

--- create_schema.sh
+++ create_schema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating user itc..."
-
+echo "Enter your password to root database: "
 PASS=$(grep \'password\': config.py | awk '{print $NF}' | grep -o -E '\w+')
 PASS="$PASS" envsubst < create_user.sql > create_user-tmp.sql
 mysql -h 127.0.0.1 -P 3306 -u root -p < create_user-tmp.sql
